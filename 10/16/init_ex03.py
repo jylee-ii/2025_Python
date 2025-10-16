df8704439b45da74418bd3a19b60086771de79cc
jylee-ii <jiyilee05@gmail.com>
2025-10-16 11:01:29 +0900

    10/16

---
+++ 10/16/init_ex03.py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self,name,stu_id):
+        #생성자: 객체 생성 시 자동 실행됨
+        self.name=name
+        self.stu_id=stu_id
+        self.att=0
+
+    def attend(self):
+        self.att+=1
+
+    def get_info(self):
+        return f"{self.stu_id}{self.name}-출석 {self.att}회"
+    
+
+s1=Student("김민수","202501")
+s2=Student("이수정","202502")
+
+s1.attend()
+s1.attend()
+s2.attend()
+
+print(s1.get_info())
+print(s2.get_info())
+
+
+           
